ceb479e9ef766b201715dd579933456c2a602f43
aMIrMXC <amirmxc@gmail.com>
2023-10-23 14:47:45 +0330

    test2

--- src/App-v2.js
+++ src/App-v2.js
@@ -3,6 +3,8 @@ import { useState } from "react";
 const faqs = [
   {
     title: "Where are these chairs assembled?",
+
+    
     text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
   },
 
@@ -16,7 +18,7 @@ const faqs = [
 
 
 
-  
+
   {
     title: "Do you ship to countries outside the EU?",
     text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
