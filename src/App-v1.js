086535ba2fcbd8140cd0f38239ed231e3f50f359
aMIrMXC <amirmxc@gmail.com>
2023-10-23 14:51:13 +0330

    t

--- src/App-v1.js
+++ src/App-v1.js
@@ -5,13 +5,17 @@ const faqs = [
 
 
 
-  
+
   {
     title: "Where are these chairs assembled?",
     text:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus."
   },
   {
+
+
+
+    
     title: "How long do I have to return my chair?",
     text:
       "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus."
